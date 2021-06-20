1f02ae46b0b98b175ca1a69d3b40387dc4ade058
Dirk Prins <info@fornext.nl>
2021-06-20 16:36:45 +0200

    Add files via upload

---
+++ Src/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CharEditor")]
+[assembly: AssemblyDescription("Character editor for Commodore 64")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ForNext")]
+[assembly: AssemblyProduct("CharEditor")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Dirk Prins")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a463fdb-eb08-441f-8ebf-b9c5002ef1b3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
